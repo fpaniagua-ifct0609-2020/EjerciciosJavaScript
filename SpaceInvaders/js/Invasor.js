d1652f19815f9d6da9f18d1503da264ef94cd553
fpaniagua <fpaniagua.ifct0609.2020@gmail.com>
2020-02-07 14:06:14 +0100

    Invasores

---
+++ SpaceInvaders/js/Invasor.js
@@ -0,0 +1,38 @@
+class Invasor {
+    
+    /*  
+    aspecto - ficherográfico
+    x, y
+    alto, ancho
+    velocidad
+
+    */
+    constructor(nombreFichero, x, y, alto, ancho, velocidad) {
+      this.aspecto = new Image();
+      this.aspecto.src = "./imagenes/" + nombreFichero;
+      this.x = x;
+      this.y = y;
+      this.alto = alto;
+      this.ancho = ancho;
+      this.velocidad = velocidad;
+    }
+
+    mover(){
+        this.x = this.x + this.velocidad;
+        if (this.x+this.ancho > c.width || this.x < 0){
+            this.velocidad = this.velocidad * -1;
+        }  
+
+        ctx.drawImage(this.aspecto, this.x, this.y, this.ancho, this.alto);
+        console.log(this.x);
+    }
+    disparar(){
+
+    }
+    bajar(){
+
+    }
+    morir(){
+
+    }
+}
---
+++ SpaceInvaders/js/Motor.js
@@ -0,0 +1,32 @@
+var c;
+var ctx;
+var antonio;
+var alberto;
+function inicializar(){
+    console.log("Inicializando...");
+    //Obtenemos la referencia al canvas (lienzo)
+    c = document.getElementById("pantalla");
+    ctx = c.getContext("2d");
+    //Pinta de negro la pantalla
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, c.width, c.height);
+    //Instanciamos el alien
+    antonio = new Invasor("invasor.png",0,0,80,80,10);
+    alberto = new Invasor("invasor.png",85,0,80,80,10);
+    fernando = new Invasor("invasor.png",170,0,80,80,10);
+    monica = new Invasor("invasor.png",255,0,80,80,10);
+    //Llamada a jugar
+    setInterval(
+        function(){
+            jugar();
+        },
+        100);
+}
+function jugar(){
+    console.log("Jugando...");
+    ctx.fillRect(0, 0, c.width, c.height);
+    antonio.mover();
+    alberto.mover();
+    fernando.mover();
+    monica.mover();
+}
